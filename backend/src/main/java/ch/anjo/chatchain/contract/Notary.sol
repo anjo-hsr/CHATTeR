1d758930f747fab10f0b967bd97b6199f19269e5
AndiHSR <andi.hoerler@hsr.ch>
2018-12-01 16:36:30 +0100

    Update contract to prevent overriding of map entry

--- backend/src/main/java/ch/anjo/chatchain/contract/Notary.sol
+++ backend/src/main/java/ch/anjo/chatchain/contract/Notary.sol
@@ -1,15 +1,25 @@
-pragma solidity >=0.4.0 <0.6.0;
+pragma solidity ^0.5.0;
 
+library Library {
+    struct data {
+        bool isValue;
+        address sender;
+    }
+}
 
 contract CHATTeR {
-   mapping(bytes32 => address) messages;
-    
+
+    using Library for Library.data;
+
+    mapping(bytes32 => Library.data) messages;
+
     function storeMessage(bytes32 messageHash) public {
-        messages[messageHash] = msg.sender;
+        require(!messages[messageHash].isValue, "Message already stored!");
+        messages[messageHash] = Library.data(true, msg.sender);
     }
-    
+
     function getSender(bytes32 messageHash) public view returns (address sender) {
-        return messages[messageHash];
+        return messages[messageHash].sender;
     }
-    
+
 }
