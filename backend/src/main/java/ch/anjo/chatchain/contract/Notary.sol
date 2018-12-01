f39e096dd3ccb56873554cb8922dc784def6142c
CHATTeR <andi.hoerler@hsr.ch>
2018-12-01 15:49:44 +0100

    Update contract

--- backend/src/main/java/ch/anjo/chatchain/contract/Notary.sol
+++ backend/src/main/java/ch/anjo/chatchain/contract/Notary.sol
@@ -1,1 +1,15 @@
-pragma solidity ^0.4.4;
+pragma solidity >=0.4.0 <0.6.0;
+
+
+contract CHATTeR {
+   mapping(bytes32 => address) messages;
+    
+    function storeMessage(bytes32 messageHash) public {
+        messages[messageHash] = msg.sender;
+    }
+    
+    function getSender(bytes32 messageHash) public view returns (address sender) {
+        return messages[messageHash];
+    }
+    
+}
