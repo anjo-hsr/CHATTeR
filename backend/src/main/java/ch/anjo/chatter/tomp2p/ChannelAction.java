907b44c079cf6ff7948d2768470b9db8acc353dc
AndiHSR <andi.hoerler@hsr.ch>
2018-12-07 16:47:48 +0100

    Fix endless loop based on function

--- backend/src/main/java/ch/anjo/chatter/tomp2p/ChannelAction.java
+++ backend/src/main/java/ch/anjo/chatter/tomp2p/ChannelAction.java
@@ -21,6 +21,7 @@ import net.tomp2p.dht.PeerDHT;
 import net.tomp2p.futures.BaseFuture;
 import net.tomp2p.futures.BaseFutureAdapter;
 import net.tomp2p.p2p.Peer;
+import net.tomp2p.p2p.RequestP2PConfiguration;
 import net.tomp2p.p2p.builder.BootstrapBuilder;
 import net.tomp2p.peers.PeerAddress;
 import net.tomp2p.storage.Data;
@@ -147,68 +148,75 @@ class ChannelAction {
     String response = JsonGenerator.generateAddPeers(chatterPeer.getChatterUser());
     ChatterUser chatterUser = chatterPeer.getChatterUser();
     webSocketMessage.chatInformation.peers.forEach(peer -> {
-      chatterPeer.getDht().get(ChatterUser.getHash(peer))
-          .start()
-          .addListener(
-              new BaseFutureAdapter<FutureGet>() {
-                @Override
-                public void operationComplete(FutureGet future) {
-                  Data data = future.data();
-                  if (!data.isEmpty()) {
-                    ChatterUser futureFriend = readUser(data);
-                    TomP2pMessage tomP2pMessage =
-                        new TomP2pMessage(
-                            chatterUser.getUsername(),
-                            Objects.requireNonNull(futureFriend).getUsername(),
-                            response);
-                    PeerAddress futureFriendAddress = futureFriend.getPeerAddress();
-                    chatterPeer.getMyself().sendDirect(futureFriendAddress).object(tomP2pMessage).start();
-                  }
+      chatterPeer.getDht()
+          .get(ChatterUser.getHash(peer))
+          .start().addListener(
+          new BaseFutureAdapter<FutureGet>() {
+            @Override
+            public void operationComplete(FutureGet future) {
+              Data data = future.data();
+              if (Objects.nonNull(data) && !data.isEmpty()) {
+                ChatterUser chatMember = readUser(data);
+                TomP2pMessage tomP2pMessage = null;
+                if (chatMember != null) {
+                  tomP2pMessage =
+                      new TomP2pMessage(
+                          chatterUser.getUsername(), peer, response);
+
+                  chatterPeer.getDht()
+                      .send(chatMember.getHash())
+                      .object(tomP2pMessage)
+                      .requestP2PConfiguration(new RequestP2PConfiguration(1, 5, 0))
+                      .start();
+
                 }
-              });
+              }
+            }
+          }
+      );
     });
   }
 
-  private static void confirmMessage(
-      ChatterPeer chatterPeer, WebSocketMessage webSocketMessage, TomP2pMessage tomP2pMessage) {
-    if (!webSocketMessage.messageInformation.author.equals(
-        chatterPeer.getChatterUser().getUsername())) {
-      String response = JsonGenerator.generateConfirmMessage(chatterPeer, webSocketMessage);
-      System.out.println(tomP2pMessage.getSender());
-      DataSender.sendWithConfirmation(chatterPeer, tomP2pMessage.getSender(), response);
+    private static void confirmMessage (
+        ChatterPeer chatterPeer, WebSocketMessage webSocketMessage, TomP2pMessage tomP2pMessage){
+      if (!webSocketMessage.messageInformation.author.equals(
+          chatterPeer.getChatterUser().getUsername())) {
+        String response = JsonGenerator.generateConfirmMessage(chatterPeer, webSocketMessage);
+        System.out.println(tomP2pMessage.getSender());
+        DataSender.sendWithConfirmation(chatterPeer, tomP2pMessage.getSender(), response);
+      }
     }
-  }
 
-  private static String getFriends(ChatterPeer chatterPeer) {
-    JsonObject responseJson = new JsonObject();
-    responseJson.addProperty(MessageTypes.TYPE_KEYWORD, MessageTypes.ADD_PEERS);
+    private static String getFriends (ChatterPeer chatterPeer){
+      JsonObject responseJson = new JsonObject();
+      responseJson.addProperty(MessageTypes.TYPE_KEYWORD, MessageTypes.ADD_PEERS);
 
-    JsonArray peers = new JsonArray();
-    Set<String> friends = chatterPeer.getChatterUser().getFriends();
+      JsonArray peers = new JsonArray();
+      Set<String> friends = chatterPeer.getChatterUser().getFriends();
 
-    Set<JsonObject> peerSet = new HashSet<>();
-    friends.forEach(
-        friend ->
-            chatterPeer
-                .getDht()
-                .get(ChatterUser.getHash(friend))
-                .start()
-                .addListener(
-                    new BaseFutureAdapter<FutureGet>() {
-                      @Override
-                      public void operationComplete(FutureGet future) {
-                        Data data = future.data();
-                        if (!data.isEmpty()) {
-                          ChatterUser friend = readUser(data);
-                          if (Objects.nonNull(friend)) {
-                            peerSet.add(friend.getInformation());
+      Set<JsonObject> peerSet = new HashSet<>();
+      friends.forEach(
+          friend ->
+              chatterPeer
+                  .getDht()
+                  .get(ChatterUser.getHash(friend))
+                  .start()
+                  .addListener(
+                      new BaseFutureAdapter<FutureGet>() {
+                        @Override
+                        public void operationComplete(FutureGet future) {
+                          Data data = future.data();
+                          if (!data.isEmpty()) {
+                            ChatterUser friend = readUser(data);
+                            if (Objects.nonNull(friend)) {
+                              peerSet.add(friend.getInformation());
+                            }
                           }
                         }
-                      }
-                    }));
+                      }));
 
-    responseJson.add("peers", peers);
+      responseJson.add("peers", peers);
 
-    return responseJson.toString();
+      return responseJson.toString();
+    }
   }
-}
