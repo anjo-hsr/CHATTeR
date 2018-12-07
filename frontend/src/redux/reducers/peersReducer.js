eabcee408dbeecee39627ab85f7263a6dbcaa1f9
AndiHSR <andi.hoerler@hsr.ch>
2018-12-07 09:00:17 +0100

    Replace existing peer if new information are given

--- frontend/src/redux/reducers/peersReducer.js
+++ frontend/src/redux/reducers/peersReducer.js
@@ -13,5 +13,16 @@ export default function reducer(reduxStore = [], action) {
 
 const mergeArrays = (originalStore, peers) => {
   const uniquePeers = peers.filter(peer => !originalStore.some(element => element.name === peer.name));
-  return originalStore.concat(uniquePeers);
+  const duplicatedPeers = peers.filter(peer => originalStore.some(element => element.name === peer.name));
+  let mergedPeers = originalStore.concat(uniquePeers);
+
+  return mergedPeers.map(peer => {
+    if (duplicatedPeers.some(element => element.name === peer.name)) {
+      const newPeerOption = duplicatedPeers.find(element => element.name === peer.name);
+      if (typeof newPeerOption.isOnline === 'boolean' && Boolean(newPeerOption.etherAddress)) {
+        peer = newPeerOption;
+      }
+    }
+    return peer;
+  });
 };
